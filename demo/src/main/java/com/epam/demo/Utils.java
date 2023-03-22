1908e09f2d18cc77b5d9d588efdfb2fe554de8a5
AlexDark <alexdarkovich@mail.ru>
2023-03-22 14:18:26 +0300

    second attempt

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,15 +2,28 @@ package com.epam.demo;
 
 import import com.epam.utils.StringUtils;
 
+package com.epam.demo;
+
 import java.util.List;
 
+import com.epam.utils.StringUtils;
+
 public class Utils {
+    public static void main(String[] args)
+    {
+        System.out.println( isAllPositiveNumbers(null));
+    }
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for(String arg : args){
-            if (!StringUtils.isPositiveNumber(arg)){
-                return false;
+        if(args == null || args.size() == 0)
+        {
+            return false;
+        } else {
+            for(String arg : args){
+                if (!StringUtils.isPositiveNumber(arg)){
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,8 +1,19 @@
 package com.epam.utils;
 
-
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         if (Double.parseDouble(str) > 0) return true;
+        if(str == null ||str.equals("null") || str == "" || str == " " || str.startsWith("0"))
+        {
+            return false;
+        } else {
+            double number = Double.parseDouble(str);
+            if (number > 0){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
     }
 }
